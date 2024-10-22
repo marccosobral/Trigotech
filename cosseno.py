994d445399c806e3c4c98cc8f664ee30c858a06d
Marccos De Araújo <marcosobral.dearaujo@gmail.com>
2024-10-22 12:16:33 -0300

    Código da trigotech

---
+++ cosseno.py
@@ -0,0 +1,12 @@
+import math
+
+# Ângulo em graus
+angulo_graus = 60
+
+# Convertendo para radianos
+angulo_radianos = math.radians(angulo_graus)
+
+# Calculando o cosseno
+cosseno = math.cos(angulo_radianos)
+
+print(f"O cosseno de {angulo_graus} graus é {cosseno}")
